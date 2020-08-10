8d13dd274d089b69722c94e5a8e2f62d2e83620f
[Fry] <[Fry@163.com]>
2020-08-10 18:03:07 +0800

    医疗app

---
+++ src/components/nav-bar.vue
@@ -0,0 +1,47 @@
+<template>
+	<van-tabbar v-model="active" class="active_tab">
+		<van-tabbar-item v-for="(item,index) in tabbars" :key="index" @click="tab(index,item.name)">
+			<span :class="currIndex == index ? active:''">{{item.title}}</span>
+			<template slot="icon" slot-scope="props">
+				<img :src="props.active ? item.active : item.normal">
+			</template>
+		</van-tabbar-item>
+	</van-tabbar>
+</template>
+
+<script>
+	export default {
+		name: "nav-bar",
+		data() {
+			return {
+				currIndex: 0,
+				active: 0,
+				tabbars: [{
+						name: "home",
+						title: "首页",
+						normal: require("@/assets/tab_home.png"),
+						active: require("@/assets/tab_home_active.png")
+					},
+					{
+						name: "category",
+						title: "分类",
+						normal: require("@/assets/tab_home.png"),
+						active: require("@/assets/tab_home_active.png")
+					},
+					{
+						name: "message",
+						title: "消息",
+						normal: require("@/assets/tab_home.png"),
+						active: require("@/assets/tab_home_active.png")
+					}
+				]
+			};
+		},
+		methods: {
+			tab(index, val) {
+				this.currIndex = index;
+				this.$router.push(val);
+			}
+		}
+	};
+</script>
---
+++ src/components/user.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="hello">
+		<van-row class="m-header">
+			<van-col span="24">
+				<van-icon name="arrow-left" class="m-header-icon" />
+				　 <p>nihao</p>
+				</van-col>
+		</van-row>
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+	.van-nav-bar {
+		background-size: 100% 100%;
+		width: 100%;
+		position: fixed;
+		z-index: 999;
+		top: 0
+	}
+
+	.van-nav-bar__title {
+		color: #fff;
+	}
+
+	.van-icon-arrow-left:before {
+		content: "\F008";
+		color: #fff;
+	}
+
+	.van-icon-user-o:before {
+		content: "\F0CC";
+		color: #fff;
+		font-size: 0.6rem;
+	}
+</style>
